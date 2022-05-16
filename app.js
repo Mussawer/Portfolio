2df044d7c3c889e0a7aa32a6f058a9f8771ab4ba
Mussawer.Raza-at-101380220521 <mussawer@gosaas.io>
2022-05-16 12:14:19 +0500

    change in server

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const path = require("path");
 
 const port = process.env.PORT || 5000;
 
-const publicPath = path.join(__dirname, "..", "out");
+const publicPath = path.join(__dirname, "out");
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'out', 'index.html'));
+  });
 
 app.listen(port, (err) => {
   if (err) {
